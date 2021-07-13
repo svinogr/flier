4f0308152ebde45909fb45c54ff4b764f574ab82
Сергей <!@>
2021-07-13 14:57:54 +0300

    refactor

---
+++ src/main/java/com/svinogr/flier/controllers/web/RegistrationCtrl.java
@@ -0,0 +1,58 @@
+package com.svinogr.flier.controllers.web;
+
+import com.svinogr.flier.model.Role;
+import com.svinogr.flier.model.User;
+import com.svinogr.flier.model.UserRole;
+import com.svinogr.flier.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@Controller
+@RequestMapping
+public class RegistrationCtrl {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public Mono<String> register(Model model) {
+        User user = new User();
+
+        return Mono.just(user).flatMap(u -> {
+            model.addAttribute("user", user);
+            return Mono.just("register");
+        });
+    }
+
+
+    @PostMapping("/register")
+    public Mono<String> saveOrUpdateUser(@Valid User user, BindingResult bindingResult, Model model ) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = CtrlUtils.getErrors(bindingResult);
+            model.addAttribute("error", errors);
+            model.addAttribute("user", user);
+            return Mono.just("register");
+        }
+// TODO проверить есть ли такой юзер
+        UserRole roleAccount = UserRole.ROLE_ACCOUNT;
+
+        Role role = new Role();
+        role.setName(roleAccount.name());
+
+        user.getRoles().add(role);
+
+       return userService.registerUser(user)
+               .flatMap(u -> Mono.just("redirect:/admin/users"))
+               .switchIfEmpty(Mono.just("redirect:/admin/users"));
+    }
+}
---
+++ src/main/java/com/svinogr/flier/controllers/web/Util.java
@@ -0,0 +1,17 @@
+package com.svinogr.flier.controllers.web;
+
+import com.svinogr.flier.model.User;
+import reactor.core.publisher.Mono;
+
+public class Util {
+    public static Mono<User> getPrincipal(){
+        //TODO изменить на настоящего юзера
+        User user = new User();
+        user.setUsername("vasya");
+        user.setEmail("vasya");
+        user.setName("vasya");
+        user.setPhone("vasya");
+        user.setSurname("vasya");
+        return Mono.just(user);
+    }
+}
