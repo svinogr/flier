e77a5f73899f89351fe98cfe32d4d0336138dc52
Сергей <!@>
2021-09-14 15:12:03 +0300

    add java doc UserRolesRepo.java

--- src/main/java/com/svinogr/flier/repo/UserRolesRepo.java
+++ src/main/java/com/svinogr/flier/repo/UserRolesRepo.java
@@ -1,16 +1,22 @@
 package com.svinogr.flier.repo;
 
-import com.svinogr.flier.model.User;
 import com.svinogr.flier.model.UserRoles;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.function.Consumer;
-
+/**
+ * @author SVINOGR
+ * version 0.0.1
+ * <p>
+ *Class Reactive repository for {@link UserRoles} implements {@link ReactiveCrudRepository}
+ */
 @Repository
 public interface UserRolesRepo extends ReactiveCrudRepository<UserRoles, Long> {
+    /**
+     * Method searching by user id
+     *
+     * @param userId value for search in column "user_id"
+     * @return found user roles. Mono<UserRoles>
+     */
     Mono<UserRoles> findByUserId(Long userId);
-
 }
