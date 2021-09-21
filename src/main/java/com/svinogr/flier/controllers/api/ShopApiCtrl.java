5fcfd01d324ea47931363037b9ed1b03f70883d3
Сергей <!@>
2021-09-21 10:08:38 +0300

    add getShopById in ShopApiCtrl.java

--- src/main/java/com/svinogr/flier/controllers/api/ShopApiCtrl.java
+++ src/main/java/com/svinogr/flier/controllers/api/ShopApiCtrl.java
@@ -6,6 +6,7 @@ import com.svinogr.flier.services.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api")
@@ -14,13 +15,24 @@ public class ShopApiCtrl {
     ShopService shopService;
 
     /**
-     * Returne json found shops by around coord
+     * Returns json found shops by around coord
      *
      * @param coord {@link Coord}
-     * @return json shops
+     * @return json shops. Flux<Shop>
      */
     @PostMapping("shop")
     public Flux<Shop> getAllShopByCoord(@RequestBody Coord coord) {
         return shopService.getAllShopsAroundCoord(coord);
+    }
+
+    /**
+     * Returns json found shops by id
+     *
+     * @param id id shop from db
+     * @return found shop. Mono<Shop>
+     */
+    @GetMapping("shop/{id}")
+    public Mono<Shop> getShopById(@PathVariable long id) {
+        return shopService.getShopById(id);
     }
 }
